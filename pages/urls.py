2511c57e58a89f0641f30e22b341dec0b0e413f2
jaejae <happyeejae02@gmail.com>
2024-08-09 01:33:06 +0900

    3주차 django 개발 만들어보기

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.mainpage),
     path('company/', views.company),
+    path('customer_service_center/', views.customer_service_center),
+    path('existing_product/', views.existing_product),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,9 @@ def mainpage(request):
 
 def company(request):
     return render(request, 'pages/company_info.html')
+
+def customer_service_center(request):
+    return render(request, 'pages/customer_service_center.html')
+
+def existing_product(request):
+    return render(request, 'pages/existing_product.html')
